6ba8b429e899a2c83b571a13de8f3814f9b2f7ad
chwd31 <chwdwile@aol.com>
2023-02-19 16:00:27 -0600

    changes to code

---
+++ generateMarkdown.js
@@ -0,0 +1,83 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+  if (!license) {
+    return '';
+  }
+  return `![${license} license](https://img.shields.io/badge/license-${license}-brightgreen)`; 
+}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+  if (!license) {
+    return '';
+  }
+
+  return `* [License](#license)\n`;
+}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+  if (!license) {
+    return '';
+  }
+
+  return `## License
+
+This project is licensed under the ${license} license.`;
+}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  const licenseBadge = renderLicenseBadge(data.license);
+  const licenseLink = renderLicenseLink(data.license);
+  const licenseSection = renderLicenseSection(data.license);
+
+  return `# ${data.title}
+
+${licenseBadge}
+
+## Description
+
+${data.description}
+
+## Table of Contents
+${licenseLink}
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+## Credits
+
+${data.credits}
+
+## Contributing
+
+${data.contributing}
+
+## Tests
+
+${data.tests}
+
+${licenseSection}
+
+## Questions
+
+If you have any questions about this project, please contact ${data.name} at ${data.email}.
+You can also find more of my projects on my GitHub page at https://github.com/${data.github}.
+`;
+}
+
